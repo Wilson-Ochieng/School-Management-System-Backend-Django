597e01e44f7cc1bce9572db2b8661173dbfea691
Wilson <wilsonochieng718@gmail.com>
2024-05-20 10:19:01 +0300

    Updated views

--- core/views.py
+++ core/views.py
@@ -15,6 +15,45 @@ class StudentViewSet(viewsets.ModelViewSet):
         if Student.objects.filter(admission_no=data['admission_no']).exists():
             return Response({'error': 'A student with this admission number already exists'}, status=status.HTTP_400_BAD_REQUEST)
         return super().create(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()  # Make a copy of the request data
+
+        # Ensure admission_no is not changed
+        data['admission_no'] = instance.admission_no
+
+        serializer = self.get_serializer(instance, data=data, partial=False)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()  # Make a copy of the request data
+
+        # Ensure admission_no is not changsed
+        data['admission_no'] = instance.admission_no
+
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+    
+    
+
+
+
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
@@ -22,10 +61,21 @@ class TeacherViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        # Check if a student with the same admission_no already exists
+        # Check if a student with the same admissi1on_no already exists
         if Teacher.objects.filter(tsc_no=data['tsc_no']).exists():
             return Response({'error': 'A Teacher with this TSC number already exists'}, status=status.HTTP_400_BAD_REQUEST)
         return super().create(request, *args, **kwargs)
+    def update(self, request, *args, **kwargs):
+        data = request.data
+        instance = self.get_object()
+        if 'tsc_no' in data and Teacher.objects.filter(tsc_no=data['tsc_no']).exclude(id=instance.id).exists():
+            return Response({'error': 'A Teacher with this TSC number already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        return super().update(request, *args, **kwargs)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
